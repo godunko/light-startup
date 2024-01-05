88331a4c487c4ade4343a31b7016b61b21cc21e3
Vadim Godunko <vgodunko@gmail.com>
2024-01-05 16:10:31 +0400

    Support of code/data initialization in ITCM/DTCM of Cortex-M7

--- source/cpu/armv7/system_armv7-startup_utilities.adb
+++ source/cpu/armv7/system_armv7-startup_utilities.adb
@@ -10,30 +10,40 @@ pragma Ada_2022;
 with Interfaces;
 with System.Storage_Elements;
 
+with System_Types;
+
 package body System_ARMv7.Startup_Utilities is
 
-   -----------------------
-   -- Copy_Data_Section --
-   -----------------------
+   procedure Copy
+     (Start_Address : System.Address;
+      End_Address   : System.Address;
+      Load_Address  : System.Address);
+   --  Copy data starting from Load_Address to the memory region at
+   --  Start_Address .. End_Address-4.
 
-   procedure Copy_Data_Section is
+   procedure Fill
+     (Start_Address : System.Address;
+      End_Address   : System.Address);
+   --  Fill memory at Start_Address .. End_Address-4 by zero.
 
+   ----------
+   -- Copy --
+   ----------
+
+   procedure Copy
+     (Start_Address : System.Address;
+      End_Address   : System.Address;
+      Load_Address  : System.Address)
+   is
       use type System.Address;
       use type System.Storage_Elements.Storage_Offset;
 
-      sidata : constant System.Storage_Elements.Integer_Address
-        with Import, Convention => C, External_Name => "_sidata";
-      sdata  : constant System.Storage_Elements.Integer_Address
-        with Import, Convention => C, External_Name => "_sdata";
-      edata  : constant System.Storage_Elements.Integer_Address
-        with Import, Convention => C, External_Name => "_edata";
-
-      Source_Address      : System.Address := sidata'Address;
-      Destination_Address : System.Address := sdata'Address;
+      Source_Address      : System.Address := Load_Address;
+      Destination_Address : System.Address := Start_Address;
 
    begin
       if Source_Address /= Destination_Address then
-         while Destination_Address /= edata'Address loop
+         while Destination_Address /= End_Address loop
             declare
                Source      : constant Interfaces.Unsigned_32
                  with Import, Address => Source_Address;
@@ -50,26 +60,72 @@ package body System_ARMv7.Startup_Utilities is
             end;
          end loop;
       end if;
+   end Copy;
+
+   -----------------------
+   -- Copy_Data_Section --
+   -----------------------
+
+   procedure Copy_Data_Section is
+      sidata : constant System.Storage_Elements.Integer_Address
+        with Import, Convention => C, External_Name => "_sidata";
+      sdata  : constant System.Storage_Elements.Integer_Address
+        with Import, Convention => C, External_Name => "_sdata";
+      edata  : constant System.Storage_Elements.Integer_Address
+        with Import, Convention => C, External_Name => "_edata";
+
+   begin
+      Copy (sdata'Address, edata'Address, sidata'Address);
    end Copy_Data_Section;
 
-   ----------------------
-   -- Fill_BSS_Section --
-   ----------------------
+   ----------------------------
+   -- Copy_DTCM_Data_Section --
+   ----------------------------
 
-   procedure Fill_BSS_Section is
+   procedure Copy_DTCM_Data_Section is
+      siitcmtext : constant System_Types.Unsigned_32
+        with Import, Convention => C, External_Name => "_siitcmtext";
+      sitcmtext  : constant System_Types.Unsigned_32
+        with Import, Convention => C, External_Name => "_sitcmtext";
+      eitcmtext  : constant System_Types.Unsigned_32
+        with Import, Convention => C, External_Name => "_eitcmtext";
+
+   begin
+      Copy (sitcmtext'Address, eitcmtext'Address, siitcmtext'Address);
+   end Copy_DTCM_Data_Section;
+
+   ----------------------------
+   -- Copy_ITCM_Text_Section --
+   ----------------------------
+
+   procedure Copy_ITCM_Text_Section is
+      sidtcmdata : constant System_Types.Unsigned_32
+        with Import, Convention => C, External_Name => "_sidtcmdata";
+      sdtcmdata  : constant System_Types.Unsigned_32
+        with Import, Convention => C, External_Name => "_sdtcmdata";
+      edtcmdata  : constant System_Types.Unsigned_32
+        with Import, Convention => C, External_Name => "_edtcmdata";
+
+   begin
+      Copy (sdtcmdata'Address, edtcmdata'Address, sidtcmdata'Address);
+   end Copy_ITCM_Text_Section;
+
+   ----------
+   -- Fill --
+   ----------
+
+   procedure Fill
+     (Start_Address : System.Address;
+      End_Address   : System.Address)
+   is
 
       use type System.Address;
       use type System.Storage_Elements.Storage_Offset;
 
-      sbss : constant System.Storage_Elements.Integer_Address
-        with Import, Convention => C, External_Name => "_sbss";
-      ebss : constant System.Storage_Elements.Integer_Address
-        with Import, Convention => C, External_Name => "_ebss";
-
-      Destination_Address : System.Address := sbss'Address;
+      Destination_Address : System.Address := Start_Address;
 
    begin
-      while Destination_Address /= ebss'Address loop
+      while Destination_Address /= End_Address loop
          declare
             Destination : Interfaces.Unsigned_32
               with Import, Address => Destination_Address;
@@ -81,6 +137,34 @@ package body System_ARMv7.Startup_Utilities is
               @ + Interfaces.Unsigned_32'Max_Size_In_Storage_Elements;
          end;
       end loop;
+   end Fill;
+
+   ----------------------
+   -- Fill_BSS_Section --
+   ----------------------
+
+   procedure Fill_BSS_Section is
+      sbss : constant System_Types.Unsigned_32
+        with Import, Convention => C, External_Name => "_sbss";
+      ebss : constant System_Types.Unsigned_32
+        with Import, Convention => C, External_Name => "_ebss";
+
+   begin
+      Fill (sbss'Address, ebss'Address);
    end Fill_BSS_Section;
+
+   ---------------------------
+   -- Fill_DTCM_BSS_Section --
+   ---------------------------
+
+   procedure Fill_DTCM_BSS_Section is
+      sdtcmbss : constant System_Types.Unsigned_32
+        with Import, Convention => C, External_Name => "_sdtcmbss";
+      edtcmbss : constant System_Types.Unsigned_32
+        with Import, Convention => C, External_Name => "_edtcmbss";
+
+   begin
+      Fill (sdtcmbss'Address, edtcmbss'Address);
+   end Fill_DTCM_BSS_Section;
 
 end System_ARMv7.Startup_Utilities;
